1566bbcf38f604780a9b819ff807018d51e90773
athulraj909 <athulrajn1999@gmail.com>
2025-02-25 17:15:45 +0530

    reglogin

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,9 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import UserData
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Email'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'})
+    )
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
 
 class UserDataForm(forms.ModelForm):
     class Meta:
